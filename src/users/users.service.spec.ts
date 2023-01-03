e9122a6ff6523633898723a298282fa8b14b3ed2
Marcus Figueiredo <figueiredo@protonmail.com>
2023-01-03 06:36:19 -0300

    test(modules/users): adds service spec

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,65 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { DataSource } from 'typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TestDataSourceConfig } from '../../orm.config';
 
 describe('UsersService', () => {
-  let service: UsersService;
+  let usersService: UsersService;
+  let databaseService: DataSource;
+  let userId: number;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [TypeOrmModule.forRoot(TestDataSourceConfig)],
       providers: [UsersService],
     }).compile();
 
-    service = module.get<UsersService>(UsersService);
+    usersService = module.get<UsersService>(UsersService);
+    databaseService = module.get<DataSource>(DataSource);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should add a new user', async () => {
+    const user = await usersService.create({
+      username: 'john.doe',
+      password: '123456',
+    });
+    userId = user.id;
+    expect(user).toHaveProperty('id');
+  });
+
+  it('should return a list of users', async () => {
+    const users = await usersService.findAll();
+    expect(users).toBeDefined();
+    expect(users.length).toBeGreaterThan(0);
+  });
+
+  it('should return a user by id', async () => {
+    const user = await usersService.findOne(userId);
+    expect(user).toBeDefined();
+    expect(user).toHaveProperty('id');
+  });
+
+  it('should return a user by username', async () => {
+    const user = await usersService.findByUsername('john.doe');
+    expect(user).toBeDefined();
+    expect(user).toHaveProperty('id');
+  });
+
+  it('should update a user by id', async () => {
+    const user = await usersService.update(userId, {
+      username: 'john.does',
+    });
+    expect(user).toHaveProperty('id');
+    expect(user.username).toBe('john.does');
+  });
+
+  it('should remove a user by id', async () => {
+    await usersService.remove(userId);
+    expect(await usersService.findOne(userId)).toBeNull();
+  });
+
+  afterAll(async () => {
+    await databaseService.destroy();
   });
 });
