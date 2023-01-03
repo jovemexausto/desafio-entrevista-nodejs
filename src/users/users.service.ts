55ab94f7c01763a6cb76f7e3ba4860f4c1d5ba83
Marcus Figueiredo <figueiredo@protonmail.com>
2023-01-03 06:55:41 -0300

    feat(modules/users): adds service implementation

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,49 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
-
   findAll() {
-    return `This action returns all users`;
+    return User.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return User.findOne({
+      where: {
+        id,
+      },
+    });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  findByUsername(username: string) {
+    return User.findOne({
+      where: {
+        username,
+      },
+    });
+  }
+
+  async create(createUserDto: CreateUserDto) {
+    const user = new User();
+    Object.assign(user, createUserDto);
+    return await user.save();
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const user = await User.findOne({
+      where: {
+        id,
+      },
+    });
+
+    Object.assign(user, updateUserDto);
+
+    return user.save();
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    return User.delete(id);
   }
 }
